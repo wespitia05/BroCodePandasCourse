157da5c94abd7c3e4f01cbaa271752bfe4c8600c
william <espiw@farmingdale.edu>
2025-10-27 20:31:14 -0400

    Bro Code pandas setup pt 1

---
+++ pandas_setup.py
@@ -0,0 +1,8 @@
+# pandas is a python library built on top of numpy
+# widely used in data analysis, data science and machine learning
+# "pandas" comes from "panel data" (has nothing to do with the animal panda :( )
+# we work with both series and data frames
+# series: one dimensional labeled column
+# data frame: two dimensional labeled grid/table
+
+# with pandas we can import, display, manipulate and export data
