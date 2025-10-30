dce6f1aab4eefc2e092e1e0510a04ccd42a714a5
william <espiw@farmingdale.edu>
2025-10-30 19:56:03 -0400

    Bro Code pandas reading our csv and printing them regularly and using to_string()

--- pandas_import_files.py
+++ pandas_import_files.py
@@ -2,3 +2,10 @@
 # json = javascript object notation
 
 import pandas as pd
+
+df = pd.read_csv("data.csv")
+
+# prints first 5 and last 5 (if csv file is too big)
+print(df)
+# prints entire csv
+print(df.to_string())
