384e47fd304c4224184f1ed173ecc78805669806
william <espiw@farmingdale.edu>
2025-10-30 20:11:30 -0400

    Bro Code pandas searching for pokemon name in data file

--- pandas_selection.py
+++ pandas_selection.py
@@ -11,11 +11,18 @@ df = pd.read_csv("data.csv", index_col="Name")
 # print(df[["Name", "Height", "Weight"]])
 
 # SELECTION BY ROW
-print(df.loc["Pikachu"])
-print(df.loc["Charizard"])
+# print(df.loc["Pikachu"])
+# print(df.loc["Charizard"])
 # prints only those two columns
-print(df.loc["Mewtwo"], ["Height", "Weight"])
+# print(df.loc["Mewtwo"], ["Height", "Weight"])
 # prints information from charizard to blastoise
-print(df["Charizard":"Blastoise"])
+# print(df["Charizard":"Blastoise"])
 # prints information from index 0 to index 11
-print(df.iloc[0:11:2, 0:3]) # last :2 means every second row, # 0:3 means only first 3 columns
+# print(df.iloc[0:11:2, 0:3]) # last :2 means every second row, # 0:3 means only first 3 columns
+
+pokemon = input("enter a pokemon name: ")
+
+try:
+    print(df.loc[pokemon])
+except KeyError:
+    print(f"{pokemon} not found")
