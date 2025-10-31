ec7f5011601c6b9e93745270a1ed024f98f78cda
william <espiw@farmingdale.edu>
2025-10-30 20:01:16 -0400

    Bro Code pandas printing out different columns individually and all together

--- pandas_selection.py
+++ pandas_selection.py
@@ -3,4 +3,8 @@ import pandas as pd
 df = pd.read_csv("data.csv")
 
 # SELECTION BY COLUMN
-print(df["Name"])
+# print(df["Name"])
+# print(df["Height"])
+# print(df["Weight"])
+# prints the selected columns
+print(df[["Name", "Height", "Weight"]])
