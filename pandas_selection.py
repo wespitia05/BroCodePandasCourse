9836492eddf25987113851fd0e22e33c5da170ac
william <espiw@farmingdale.edu>
2025-10-30 20:09:18 -0400

    Bro Code pandas renaming indexes to be the name of the pokemon and selecting data by row, both individually and multiples

--- pandas_selection.py
+++ pandas_selection.py
@@ -1,10 +1,21 @@
 import pandas as pd
 
-df = pd.read_csv("data.csv")
+# sets the index for each row to be the pokemon's name
+df = pd.read_csv("data.csv", index_col="Name")
 
 # SELECTION BY COLUMN
 # print(df["Name"])
 # print(df["Height"])
 # print(df["Weight"])
 # prints the selected columns
-print(df[["Name", "Height", "Weight"]])
+# print(df[["Name", "Height", "Weight"]])
+
+# SELECTION BY ROW
+print(df.loc["Pikachu"])
+print(df.loc["Charizard"])
+# prints only those two columns
+print(df.loc["Mewtwo"], ["Height", "Weight"])
+# prints information from charizard to blastoise
+print(df["Charizard":"Blastoise"])
+# prints information from index 0 to index 11
+print(df.iloc[0:11:2, 0:3]) # last :2 means every second row, # 0:3 means only first 3 columns
