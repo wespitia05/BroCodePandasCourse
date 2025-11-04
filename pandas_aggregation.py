933c55f54b4d60c4ffb6aa4ba4a0db7cfcdd5719
william <espiw@farmingdale.edu>
2025-11-03 22:48:18 -0500

    Bro Code pandas finding maximum and count values from all numeric columns

--- pandas_aggregation.py
+++ pandas_aggregation.py
@@ -6,9 +6,14 @@ import pandas as pd
 
 df = pd.read_csv("data.csv")
 
+# THESE AGGREGATE FUNCTIONS APPLY TO WHOLE DATAFRAMES
 # tells pandas to find the mean of any columns that are numeric
 print(df.mean(numeric_only=True))
 # tells pandas to find the sum of any columns that are numeric
 print(df.sum(numeric_only=True))
 # tells pandas to find the minimum value from all numeric columns
 print(df.min(numeric_only=True))
+# tells pandas to find the maximum value from all numeric columns
+print(df.max(numeric_only=True))
+# tells pandas to count the number of numeric values from each column
+print(df.count(numeric_only=True))
