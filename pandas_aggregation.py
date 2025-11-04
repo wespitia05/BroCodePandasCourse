d4fcb4cc89b2dfe5881f5293f562972e842fe1ef
william <espiw@farmingdale.edu>
2025-11-03 22:46:37 -0500

    Bro Code pandas finding sum and minimum value from the numeric columns

--- pandas_aggregation.py
+++ pandas_aggregation.py
@@ -8,3 +8,7 @@ df = pd.read_csv("data.csv")
 
 # tells pandas to find the mean of any columns that are numeric
 print(df.mean(numeric_only=True))
+# tells pandas to find the sum of any columns that are numeric
+print(df.sum(numeric_only=True))
+# tells pandas to find the minimum value from all numeric columns
+print(df.min(numeric_only=True))
