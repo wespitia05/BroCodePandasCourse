0de6c5858909085d158eecd426b5b22901d12e29
william <espiw@farmingdale.edu>
2025-11-03 22:54:46 -0500

    Bro Code aggregation using the groupby function to print values based on the group we choose

--- pandas_aggregation.py
+++ pandas_aggregation.py
@@ -29,3 +29,13 @@ print(df["Height"].min())
 print(df["Height"].max())
 # tells pandas to count the number of values from the height column only
 print(df["Height"].count())
+
+# GROUPBY FUNCTION
+# groups by Type1
+group = df.groupby("Type1")
+
+print(group["Height"].mean())
+print(group["Height"].sum())
+print(group["Height"].min())
+print(group["Height"].max())
+print(group["Height"].count())
